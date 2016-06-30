d0d6d24e3e4225b823237b926ee839428bec0cdf
Ruo Zhang <zhangruo1117@gatech.edu>
2016-06-30 16:58:07 +0800

    Create Answer.java

---
+++ Answer.java
@@ -0,0 +1,82 @@
+package com.google.challenges; 
+import java.util.*;
+
+public class Answer {   
+    public static String[] answer(String[] names) { 
+
+        // Your code goes here.
+        //String[] sorted =  new String[names.length];
+       // int counter = 0;
+        
+        //while((counter) < names.length)
+        //{
+         //   String temp = findBiggest(names);
+         //   sorted[counter] = temp;
+         //   counter ++ ;
+        //}
+        
+        //for(int i = 0; i < sorted.length; i++)
+        //{
+          //   System.out.println(sorted[i]);
+        //}
+        Arrays.sort(names, new scoreComparator());
+
+        return names;
+
+    } 
+    
+    private static class scoreComparator implements java.util.Comparator<String> {
+        @Override
+        public int compare(String s1, String s2) {
+            int val1 = calculateScore(s1);
+            int val2 = calculateScore(s2);
+            if(val1 > val2)
+                return -1;
+
+            if(val2 > val1)
+                return 1;
+
+            // Handles the same score case and compares for the lexicographically larger name.
+            if(val1 == val2){
+                return calculateLexi(s1,s2);
+            }
+
+            return 0;
+        }
+    }
+    
+    public static String findBiggest(String[] names)
+    {
+        String max = "";
+        int index = 0;
+        for (int i = 1; i < names.length; i++) {
+            if (calculateScore(names[i]) >= calculateScore(max)) {
+               
+               if(calculateLexi(names[i], max)<0)
+                    {   
+                        max = names[i];
+                        index  = i;
+                        
+                    }
+            }
+        }
+        names[index] = "";
+        return max;
+    }
+    
+    
+    public static int calculateScore(String name)
+    {
+        int sum = 0;
+        for (int i = 0; i < name.length(); i++) {
+            char c = name.charAt(i);
+            sum = sum + (c - 'a' + 1);
+        }
+        return sum;
+    }
+    
+    public static int calculateLexi(String name1, String name2)
+    {
+        return name1.compareTo(name2);
+    }
+}
