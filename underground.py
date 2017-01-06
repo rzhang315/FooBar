3ef8ca3b0442797702569571c5b5bdb3b350be14
Ruo Zhang <zhangruo1117@gatech.edu>
2017-01-06 17:30:09 +0900

    Update underground.py

--- underground.py
+++ underground.py
@@ -1,7 +1,3 @@
-#mem_graphs = {}
-mem_counts = {}
-mem_choose = {}
-
 def memoize(f):
     
     class memodict(dict):
@@ -13,20 +9,6 @@ def memoize(f):
             ret = self[key] = self.f(*key)
             return ret
     return memodict(f)
-
-#@memoize  
-#def possible_graphs(n, k):
-    """
-    Returns the total number of graphs with that can be formed using
-    n nodes and k vertices. This includes graphs that are
-    identical for undirected labelled graphs, as well as
-    unconnected graphs.
-    This function effectively returns the number of ways you can
-    choose k vertices out of the 'n choose 2' possible choices.
-    """
-#    return choose(n * (n - 1) >> 1, k)
-    
-
 
 @memoize    
 def choose(n, k):
@@ -48,36 +30,25 @@ def choose(n, k):
 def count(n, k):
 
     if k == n - 1:
-        # Cayley's formula
         c = int(n ** (n - 2))
     else:
-        
-        # number of possible vertices
         p = n * (n - 1) >> 1
 
         if k == p:
-            # only way is to connect each node to all other nodes,
-            # therefore only a single distinct graph
+            # only one way
             c = 1
-
         else:
-
-            # initially all possible graphs
             c = choose(p, k)
 
-            # there can only be duplicates or unconnected components
-            # if the number of nodes is less than p - n + 2.
-            # equivalent of k < (n - 1 choose 2)
-            if k < p - n + 2:
-
+            if k < p - n + 2: # choose(n - 1, 2): for ex, maybe one unconnected from network
+                #find such unconnected sums
                 for i in range(1, n):
                     x = choose(n - 1, i - 1)
-
-                    # minimum of possible vertices for 'i' nodes and 'k'
                     y = min(i * (i - 1) >> 1, k)
 
                     for j in range(i - 1, y + 1):
-                        # exclude invalid graphs from the total
+                        # formula from https://oeis.org/A001187/a001187.pdf
+                        # to calculate number of labeled graphs
                         c -= x * choose((n - i) * ((n - i) - 1) >> 1, (k - j)) * count(i, j)
 
     return c
