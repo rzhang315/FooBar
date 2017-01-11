5f538a0c699fdd3671e5195333b651e282634aa4
Ruo Zhang <zhangruo1117@gatech.edu>
2017-01-11 12:49:54 +0900

    Update carrotland.py

--- carrotland.py
+++ carrotland.py
@@ -9,11 +9,10 @@ def getNumPosLine(Ax, Bx, Ay, By):
         y = abs(Ay - By)
         return ( GCD(x,y) + 1 )
       
-    
+#greatest common denominator   
 def GCD( x,  y):
         return x if y == 0 else GCD(y, x%y)
-        #return y == 0 ? x : GCD(y, x%y)
-    
+
     
 def parallelOrPerpendicular(Ax,  Ay,  Bx,  By):
         return ( (Ax == Bx) or (Ay == By) )
@@ -29,62 +28,57 @@ def answer(vertices):
     verticalRange = abs(upperBound - lowerBound)
 
     answer =  ( horizontalRange - 1 ) * ( verticalRange - 1 )
-    #print ("anws", answer)
+    #point inside the rectangle
 
     for poIndex in range(0, 3):
-         #   // first po
+         #  
          Ax = vertices[poIndex][0]
          Ay = vertices[poIndex][1]
             
-         #   // second po
+         #  
          Bx = vertices[(poIndex+1)%3][0]
          By = vertices[(poIndex+1)%3][1]
             
-         #   // is line perpendicular or parallel
-         #   // to the X-axis
+         #  skip when perpendicular or parallel
+
          if(parallelOrPerpendicular(Ax, Ay, Bx, By)): 
                 continue
             
             
          numPtsLine = getNumPosLine(Ax, Bx, Ay, By)
             
-         #   exclude endpos
+         #   excludes end points
          numPtsLine -= 2
             
          width = abs(Ax - Bx)
          height = abs(Ay - By)
             
          num = ( (width - 1) * (height - 1) ) - numPtsLine
-            
+         #   excludes redundant triangle 
          answer -= numPtsLine + num/2
-        
-         #print ("anws", answer)
+           
+           
+    #// Subtract the double-counted area from the corner, draw it u know it       
+         
     for a in range(0, 3): 
             x = vertices[a][0]
             y = vertices[a][1]
-
+            # skip when it is a corner 
             if (x == leftBound or x == rightBound or y == upperBound or y == lowerBound): 
                 continue
-            
 
-            #// If this po is inside the rectangle, it must mean that
-            #// the other two pos are the corners of the rectangle
             edge1 = vertices[(a + 1) % 3]
             edge2 = vertices[(a + 2) % 3]
 
-            #// other two corners
             edge3 = [edge1[0], edge2[1]]
             edge4 = [edge2[0], edge1[1]] 
 
-            #// need to find which corner this po is closer to,
-            #// since the contained rectangle is not a part of the triangle.
             distance1 = distance(x, y, edge3[0], edge3[1])
             distance2 = distance(x, y, edge4[0], edge4[1])
 
             edge = edge3 if (distance1 < distance2) else edge4
-            #edge = distance1 < distance2 ? edge3 : edge4
 
-            #// Subtract the double-counted area from the corner
+
             width = abs(edge[0] - x)
             height = abs(edge[1] - y)
 
